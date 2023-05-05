26a6ac098d8adc07f54248d2831497406ddaaafb
fraude221 <60751298+fraude221@users.noreply.github.com>
2023-05-05 20:49:02 +0300

    Seperated Graphs and Tables

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import pandas as pd
 import customtkinter
 from tkinter import filedialog
 import matplotlib.pyplot as plt
-import matplotlib.gridspec as grid_spec
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 customtkinter.set_appearance_mode("dark")
@@ -17,95 +16,120 @@ class App(customtkinter.CTk):
 
     # configure window
     self.title("CustomTkinter complex_example.py")
-    self.geometry(f"{1100}x{580}")
+    self.geometry(f"{1200}x{800}")
 
     # configure grid layout (4x4)
     self.grid_columnconfigure(1, weight=1)
-    self.grid_columnconfigure((2, 3), weight=0)
-    self.grid_rowconfigure((0, 1, 2), weight=1)
+    self.grid_rowconfigure(0, weight=1)
 
     # create sidebar frame with widgets
     self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
     self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
     self.sidebar_frame.grid_rowconfigure(4, weight=1)
-    self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="COP Feature Extraction Tool",
-                                             font=customtkinter.CTkFont(size=20, weight="bold"))
+
+    self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="COP Feature Extraction Tool", font=customtkinter.CTkFont(size=20, weight="bold"))
     self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
-    self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame,text="Upload Excel File", width=250, height=50, command=self.upload_file)
-    self.sidebar_button_1.grid(row=1, column=0, padx=20, pady=10)
+
+    self.sidebar_upload_button = customtkinter.CTkButton(self.sidebar_frame,text="Upload Excel File", width=250, height=50, command=self.upload_file)
+    self.sidebar_upload_button.grid(row=1, column=0, padx=20, pady=10)
+
     self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w")
     self.appearance_mode_label.grid(row=7, column=0, padx=20, pady=(10, 0))
-    self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame,
-                                                                   values=["Light", "Dark"],
-                                                                   command=self.change_appearance_mode_event)
-    self.appearance_mode_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 20))
+    self.appearance_mode_option_menu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["Light", "Dark"], command=self.change_appearance_mode_event)
+    self.appearance_mode_option_menu.grid(row=8, column=0, padx=20, pady=(10, 20))
 
     # create tabview
-    self.tabview = customtkinter.CTkTabview(self, width=800)
-    self.tabview.grid(row=0, column=1, rowspan=4, padx=(20, 20), pady=(20, 20), sticky="nsew")
+    self.tabview = customtkinter.CTkTabview(self)
+    self.tabview.grid(row=0, column=1, padx=(20, 20), pady=(20, 20), sticky="nsew")
+    self.tabview.grid_columnconfigure(0, weight=1)
     self.tabview.add(tab1)
     self.tabview.add(tab2)
     self.tabview.tab(tab1).grid_columnconfigure(0, weight=1)
+    self.tabview.tab(tab1).grid_rowconfigure((0,1), weight=1)
     self.tabview.tab(tab2).grid_columnconfigure(0, weight=1)
 
+    # create graph view
+    self.tabview_graph_frame_up = customtkinter.CTkFrame(self.tabview.tab(tab1), corner_radius=0)
+    self.tabview_graph_frame_up.grid(row=0, column=0, padx=(20, 20), pady=(10, 10), sticky="nsew")
+
+    self.tabview_graph_frame_down = customtkinter.CTkFrame(self.tabview.tab(tab1), corner_radius=0)
+    self.tabview_graph_frame_down.grid(row=1, column=0, padx=(20, 20), pady=(10, 10), sticky="nsew")
+
     # create scrollable frame
-    self.scrollable_frame = customtkinter.CTkScrollableFrame(self.tabview.tab(tab2), height=800)
-    self.scrollable_frame.grid(row=0, column=0,rowspan=4, padx=(20, 20), pady=(20, 20), sticky="nsew")
-    self.scrollable_frame.grid_columnconfigure(0, weight=1)
+    self.tabview_scrollable_frame = customtkinter.CTkScrollableFrame(self.tabview.tab(tab2), height=800)
+    self.tabview_scrollable_frame.grid(row=0, column=0, padx=(20, 20), pady=(20, 20), sticky="nsew")
 
     # set default values
-    self.appearance_mode_optionemenu.set("Dark")
+    self.appearance_mode_option_menu.set("Dark")
 
   def change_appearance_mode_event(self, new_appearance_mode: str):
     customtkinter.set_appearance_mode(new_appearance_mode)
 
   def upload_file(self):
-    fig = plt.figure(constrained_layout=True)
-    fig.set_size_inches(8,6)
-
     file_path = filedialog.askopenfilename(filetypes=[("Excel file", "*.xlsx")])
-    print(file_path)
     df = pd.read_excel(file_path)
 
-    time1 = df['Time']
-    x1 = df['COPx']
-    y1 = df['COPy']
+    time = df['Time']
+    x = df['COPx']
+    y = df['COPy']
 
+    self.generate_scatter_plot(x, y, time)
+    self.generate_line_plot(x, y)
+    self.generate_tables()
+
+  def generate_scatter_plot(self, x, y, time):
+    # Clear grid widgets
+    for widget in self.tabview_graph_frame_up.winfo_children():
+      widget.destroy()
+
+    # Add the first scatter plot to the top left cell
+    fig, ax = plt.subplots()
+    sc = ax.scatter(x, y, c=time, cmap='RdBu_r', edgecolor='black', linewidths=1, alpha=0.75)
+    ax.set_title('Scatter Plot')
+    ax.set_xlabel('COPx')
+    ax.set_ylabel('COPy')
+    fig.colorbar(sc, shrink=0.6, label='time')
+
+    canvas = FigureCanvasTkAgg(fig, master=self.tabview_graph_frame_up)
+    canvas.get_tk_widget().grid(row=0, column=0, sticky="ns")
+    self.tabview_graph_frame_up.grid_rowconfigure(0, weight=1)
+    self.tabview_graph_frame_up.grid_columnconfigure(0, weight=1)
+    canvas.draw()
+
+  def generate_line_plot(self, x, y):
+    # Clear grid widgets
+    for widget in self.tabview_graph_frame_down.winfo_children():
+      widget.destroy()
+
+    fig, ax = plt.subplots()
+    ax.plot(x, y)
+    ax.set_title('Line Plot')
+
+    canvas = FigureCanvasTkAgg(fig, master=self.tabview_graph_frame_down)
+    canvas.get_tk_widget().grid(row=0, column=0, sticky="ns")
+    self.tabview_graph_frame_down.grid_rowconfigure(0, weight=1)
+    self.tabview_graph_frame_down.grid_columnconfigure(0, weight=1)
+    canvas.draw()
+
+  def generate_tables(self):
     # Define the table data and column labels
     table_data_1 = [['Label 1', 'Value 1'], ['Label 2', 'Value 2'], ['Label 3', 'Value 3']]
     col_labels_1 = ['Label', 'Value']
 
-    # Clear the previous figure and create a new 2x2 grid layout
-    fig.clear()
-    grid = grid_spec.GridSpec(nrows=2, ncols=1, figure=fig)
-    grid2 = grid_spec.GridSpecFromSubplotSpec(nrows=1, ncols=2, subplot_spec=grid[0])
-
-    # Add the first scatter plot to the top left cell
-    ax1 = fig.add_subplot(grid2[0])
-    sc = ax1.scatter(x1, y1, c=time1, cmap='RdBu_r', edgecolor='black', linewidths=1, alpha=0.75)
-    ax1.set_title('Scatter Plot 1')
-    ax1.set_xlabel('COPx')
-    ax1.set_ylabel('COPy')
-
-    fig.colorbar(sc, shrink=0.6, label='time')
-
-    ax2 = fig.add_subplot(grid2[1])
-    ax2.plot(x1, y1)
-    ax2.set_title('Scatter Plot 2')
-
     # Add the first table to the top right cell
-    ax3 = fig.add_subplot(grid[1])
-    table1 = ax3.table(cellText=table_data_1, colLabels=col_labels_1, loc='center')
-    table1.auto_set_font_size(False)
-    table1.set_fontsize(12)
-    table1.scale(1, 1.5)
-    ax3.axis('off')
+    fig, ax = plt.subplots()
+    table = ax.table(cellText=table_data_1, colLabels=col_labels_1, loc='center')
+    table.auto_set_font_size(False)
+    table.set_fontsize(12)
+    table.scale(1, 1.5)
+    ax.axis('off')
 
-    # Redraw the canvas with the updated figure
-
-    canvas = FigureCanvasTkAgg(fig, master=self.tabview.tab("Graphs"))
+    canvas = FigureCanvasTkAgg(fig, master=self.tabview_scrollable_frame)
+    canvas.get_tk_widget().grid(row=0, column=0, sticky="nsew")
+    self.tabview_scrollable_frame.grid_rowconfigure(0, weight=1)
+    self.tabview_scrollable_frame.grid_columnconfigure(0, weight=1)
     canvas.draw()
-    canvas.get_tk_widget().place(relx=0.06, rely=0)
+
 
 # Start the main loop
 if __name__ == "__main__":
